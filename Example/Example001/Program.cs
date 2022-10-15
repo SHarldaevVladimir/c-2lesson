85b3051f79c0316631ade5ae3a18803630821d81
VladimirSharldaev <vladimirsarldaev43246@gmail.com>
2022-10-15 13:27:33 +0300

    add example001 fix1

--- Example/Example001/Program.cs
+++ Example/Example001/Program.cs
@@ -16,9 +16,10 @@ int c1 = 45;
 int c2 = 56;
 int c3 = 25;
 
-int max1 = Max(a1,b1,c1);
-int max2 = Max(a2,b2,c2);
-int max3 = Max(a3,b3,c3);
-int max = Max(max1,max2,max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
